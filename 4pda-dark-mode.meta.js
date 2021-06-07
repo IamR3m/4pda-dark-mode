5b8238a26000f0fb0f3d857e186d1debe40c01c2
IamR3m <klubberzone@gmail.com>
2021-06-07 18:12:39 +0300

    Update 4pda-dark-mode.meta.js

--- 4pda-dark-mode.meta.js
+++ 4pda-dark-mode.meta.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         4pda Dark Mode
-// @namespace    https://4pda.to/forum/index.php
-// @version      0.5.3
+// @namespace    4PDA
+// @homepage     https://4pda.to/forum/index.php?showtopic=1026245
+// @version      0.5.4
 // @description  Dark Mode to 4pda
 // @author       IamR3m
 // @match        https://4pda.ru/*
@@ -9,5 +10,6 @@
 // @icon         https://ds-assets.cdn.devapps.ru/cQMtfz1ctAlz1Rhz2XO6lH.png
 // @downloadURL  https://github.com/IamR3m/4pda-dark-mode/raw/main/4pda-dark-mode.user.js
 // @updateURL    https://github.com/IamR3m/4pda-dark-mode/raw/main/4pda-dark-mode.meta.js
+// @run-at document-start
 // @grant        none
 // ==/UserScript==
