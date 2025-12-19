1dce3757b36d3189c1b75127293acb87821e8832
IamR3m <klubberzone@gmail.com>
2025-12-19 10:15:07 +0300

    Update publish.sh

--- .github/publish.sh
+++ .github/publish.sh
@@ -10,19 +10,13 @@ while IFS= read -r line; do
 done <<< "$comment"
 post+="[/list]"
 
-curl 'https://4pda.to/forum/index.php' \
---header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.9,application/signed-exchange;v=b3;q=0.7' \
---header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.00 Safari/537.36' \
---header "Cookie: $COOKIE" \
---form 'act=post' \
---form 'CODE=03' \
---form "f=$FORUM" \
---form "t=$TOPIC" \
---form "auth_key=$AUTH_KEY" \
---form "post=$post" \
---form "topic_id=$TOPIC" \
---form 'enable-emo-sig-flag=1' \
---form 'enableemo=yes' \
---form 'enablesig=yes' \
---form 'mod_options=nowt' \
---form 'submit='
+json=$(jq -nc \
+  --arg forum "$FORUM" \
+  --arg topic "$TOPIC" \
+  --arg post "$post" \
+  '{forum:$forum, topic:$topic, post:$post}')
+
+curl 'https://iamr3m.icu:3008/signal' \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${SIGNAL_SECRET}" \
+  --data "$json"
